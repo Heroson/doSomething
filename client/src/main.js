dd572b03091b54e53f8451ede59fd9d21ec78b34
Heroson <573312203@qq.com>
2018-07-10 08:25:16 +0800

    add reference project —— vue2-happyfri

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import ElementUI from 'element-ui'
 
 import routers from './router/router'
-import store from './store'
+import store from './store/'
 import ajax from './utils/ajax'
-import './config/rem'
+import './utils/rem'
 import './style/reset'
 
 Vue.use(VueRouter)
---
+++ client/src/page/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home_container">
+    <<!-- itemcontainer father-component></itemcontainer> -->
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+  import itemcontainer from '../../components/itemcontainer'
+
+  export default {
+    name: 'home',
+    components: {
+      itemcontainer
+    },
+    created(){
+
+    }
+  }
+</script>
